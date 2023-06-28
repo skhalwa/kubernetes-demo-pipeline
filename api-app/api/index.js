a27b0e409354d25df3c34827c9dad4f4e93a7798
shubham <shubhamkumarhalwa@gmail.com>
2023-06-28 11:41:09 +0530

    update

--- api-app/api/index.js
+++ api-app/api/index.js
@@ -3,11 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   console.log('working fine on Staging')
-<<<<<<< HEAD
   res.send('Api application working fine, new updated');
-=======
-  res.send('Api application working fine, updated');
->>>>>>> 240dff53c2bda13baae31377d81b3e4071fdd02e
 });
 
 const port = process.env.PORT || 3000; 
