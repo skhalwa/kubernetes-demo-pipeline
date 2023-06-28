656a1a6ae0104bff2034dff2143f51d4fb5b9acd
shubham <shubhamkumarhalwa@gmail.com>
2023-06-28 16:07:01 +0530

    update

--- node-app/node/index.js
+++ node-app/node/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   console.log('Node application working fine')
-  res.send('Node api updated.');
+  res.send('Node api updated...new update');
 });
 
 const port = process.env.PORT || 5000;
